395615363398211f5dad0991449e7e063e362c1d
Irina Kudryavtseva <9173892910q@gmail.com>
2024-04-30 15:20:43 +0500

    programm1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = {"1234", "19dd", "-2", "computer science", "a98"};
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <= 3) 
+{
+    Console.WriteLine(array[i]);
+}
+}  
